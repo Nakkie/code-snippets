8e206fed156292b7e0a2e4b2f061f4575c04a6f9
Nakkie <32536982+Nakkie@users.noreply.github.com>
2022-12-14 08:57:13 +0200

    Create basic_venv.sh
    Cause I'm lazy

---
+++ Python/basic_venv.sh
@@ -0,0 +1,15 @@
+# create
+$python -m venv \path\to\myenv
+
+# activate
+bash/zsh
+$ source <venv>/bin/activate
+
+cmd.exe
+C:\> <venv>\Scripts\activate.bat
+
+PowerShell
+PS C:\> <venv>\Scripts\Activate.ps1
+
+# deactivate
+deactivate
