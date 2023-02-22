1195b71dd573e63aee07ccbc3c33f796d8be85f2
Nakkie <32536982+Nakkie@users.noreply.github.com>
2023-02-22 08:32:39 +0200

    Update dim_date.sql
    interim improvement

--- BQ/dim_date.sql
+++ BQ/dim_date.sql
@@ -1,8 +1,16 @@
-with dimdate as
-(
-select distinct Dates
-               ,FORMAT_DATE('%B %E4Y', Dates) as DisplayName
-               ,FORMAT_DATE('%E4Y%m', Dates) as OrderMonth
-               ,FORMAT_DATE('%E4Y', Dates) as OrderYear
-from unnest(GENERATE_DATE_ARRAY("2016-06-01", CURRENT_DATE())) as Dates
-),
+select
+  date,
+  EXTRACT(YEAR FROM date) AS year,
+  EXTRACT(QUARTER FROM date) AS quarter,
+  EXTRACT(MONTH FROM date) AS month,
+  EXTRACT(WEEK FROM date) AS week,
+  EXTRACT(DAY FROM date) AS day,
+  EXTRACT(DAYOFWEEK FROM date) AS day_of_week,
+  EXTRACT(DAYOFYEAR FROM date) AS day_of_year,
+  FORMAT_DATE('%Y-%m', date) AS year_month,
+  FORMAT_DATE('%Y-%m-%d', date) AS date_id,
+  CAST(FORMAT_DATE('%Y%m%d', date) AS INT64) AS int_date_id,
+  CAST(FORMAT_DATE('%Y%W', date) AS INT64) AS int_week_id,
+  CAST(FORMAT_DATE('%Y', date) AS INT64) * 10000 + CAST(FORMAT_DATE('%W', date) AS INT64) AS week_id
+from
+  UNNEST(GENERATE_DATE_ARRAY('2021-01-01', '2022-12-31')) AS date;
