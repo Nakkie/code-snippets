62737b3b97b0182d6eff8b0b8363d686bc7935e2
Nakkie <32536982+Nakkie@users.noreply.github.com>
2023-03-28 18:55:39 +0200

    Update dim_date.sql

--- BQ/dim_date.sql
+++ BQ/dim_date.sql
@@ -1,15 +1,15 @@
 select
-  date,
-  EXTRACT(YEAR FROM date) AS year,
-  EXTRACT(QUARTER FROM date) AS quarter,
-  EXTRACT(MONTH FROM date) AS month,
-  EXTRACT(WEEK FROM date) AS week,
-  EXTRACT(DAY FROM date) AS day,
-  EXTRACT(DAYOFWEEK FROM date) AS day_of_week,
-  EXTRACT(DAYOFYEAR FROM date) AS day_of_year,
-  FORMAT_DATE('%Y-%m', date) AS year_month,
-  FORMAT_DATE('%Y-%m-%d', date) AS date_id,
-  CAST(FORMAT_DATE('%Y%m%d', date) AS INT64) AS int_date_id,
-  CAST(FORMAT_DATE('%Y%W', date) AS INT64) AS int_week_id,
-  CAST(FORMAT_DATE('%Y', date) AS INT64) * 10000 + CAST(FORMAT_DATE('%W', date) AS INT64) AS week_id
+  Dates,
+  EXTRACT(YEAR FROM Dates) AS year,
+  EXTRACT(QUARTER FROM Dates) AS quarter,
+  EXTRACT(MONTH FROM Dates) AS month,
+  EXTRACT(WEEK FROM Dates) AS week,
+  EXTRACT(DAY FROM Dates) AS day,
+  EXTRACT(DAYOFWEEK FROM Dates) AS day_of_week,
+  EXTRACT(DAYOFYEAR FROM Dates) AS day_of_year,
+  FORMAT_DATE('%Y-%m', Dates) AS year_month,
+  FORMAT_DATE('%Y-%m-%d', Dates) AS date_id,
+  CAST(FORMAT_DATE('%Y%m%d', Dates) AS INT64) AS int_date_id,
+  CAST(FORMAT_DATE('%Y%W', Dates) AS INT64) AS int_week_id,
+  CAST(FORMAT_DATE('%Y', Dates) AS INT64) * 10000 + CAST(FORMAT_DATE('%W', Dates) AS INT64) AS week_id
 from unnest(GENERATE_DATE_ARRAY("2016-06-01", CURRENT_DATE())) as Dates
