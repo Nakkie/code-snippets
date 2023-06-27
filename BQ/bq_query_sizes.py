0795b37a1180585aa1f882d24d56b7c1988915ea
HardusDM <45653984+HardusDM@users.noreply.github.com>
2023-06-27 12:53:49 +0200

    Update bq_query_sizes.py
    suggested updates by users

--- BQ/bq_query_sizes.py
+++ BQ/bq_query_sizes.py
@@ -11,12 +11,12 @@ client = bigquery.Client(project=project_name)
 regions = ['region-eu','region-us']
 
 output_file = 'C:\\Gitprojects\\{}_querysizes.csv'.format(project_name)
-main_df = pd.DataFrame(columns=['region', 'creation_time', 'user_email', 'job_type', 'statement_type', 'total_bytes_processed', 'total_slot_ms', 'cache_hit', 'total_bytes_billed'])
+main_df = pd.DataFrame(columns=['region', 'creation_time', 'user_email', 'job_id', 'job_type', 'statement_type', 'query', 'total_bytes_processed', 'total_slot_ms', 'cache_hit', 'total_bytes_billed'])
 
 if regions:
     for region in regions:  # API request(s)
 
-        query_job = client.query("select creation_time, user_email, job_type, statement_type, total_bytes_processed, total_slot_ms, cache_hit, total_bytes_billed/pow(1024,3) as total_gb_billed from "+region+".INFORMATION_SCHEMA.JOBS")
+        query_job = client.query("select creation_time, user_email, job_id, job_type, statement_type, query, total_bytes_processed, total_slot_ms, cache_hit, total_bytes_billed/pow(1024,3) as total_gb_billed from "+region+".INFORMATION_SCHEMA.JOBS")
 
         results = query_job.result()
         for row in results:
